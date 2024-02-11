005f2714469bfbe953b5c469571291f00543f5b8
leo.wu <leowu102000@gmail.com>
2024-02-12 00:04:10 +0800

    finish python ui

--- turtlebot_test/src/python_pub_sub/python_ui/ros_ui.py
+++ turtlebot_test/src/python_pub_sub/python_ui/ros_ui.py
@@ -10,32 +10,36 @@ from rclpy.node import Node
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
 
 class MinimalPublisher(Node):
 
     def __init__(self, send_back_function):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.vel_pub   = self.create_publisher(Twist, 'cmd_vel', 10)
         timer_period = 0.5  # seconds
         self.subscription = self.create_subscription(
-            String,
-            'topic',
+            Odometry,
+            'odom',
             self.listener_callback,
             10)
         self.subscription  # prevent unused variable warning
         self.send_back_function = send_back_function
         
         self.i = 0
-    def pub_message(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+    def pub_message(self,speed):
+        msg = Twist()
+        
+        msg.linear.x  = speed;
+        msg.angular.z = 0.0;
+  
+        self.vel_pub.publish(msg);
+        print("pub speed is "+str(speed))
         
     def listener_callback(self, msg):
         
-        self.send_back_function(msg.data)
+        self.send_back_function(msg.pose.pose.position.x,msg.pose.pose.position.y)
         
 class RunningThread(QThread):
     def __init__(self, parent, signal):
@@ -48,15 +52,15 @@ class RunningThread(QThread):
 
         rclpy.spin(self.minimal_publisher)
         rclpy.shutdown()
-    def pub_message_by_thread(self):
-        self.minimal_publisher.pub_message()
+    def pub_message_by_thread(self,speed):
+        self.minimal_publisher.pub_message(speed)
         
-    def send_back_to_main_ui(self,msd_data):
+    def send_back_to_main_ui(self,positionX,positionY):
         
-        self.signal.emit(msd_data)
+        self.signal.emit(positionX,positionY)
         
 class MainWindow(QMainWindow):
-    signal = pyqtSignal(str)
+    signal = pyqtSignal(float,float)
     def __init__(self):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
@@ -65,17 +69,34 @@ class MainWindow(QMainWindow):
         self.signal.connect(self.show_message)
         self.runningThread = RunningThread(self,self.signal)
         self.runningThread.start()
+        self.speed = 0
         
     def ui_connection(self):
-        self.setIDButton = self.ui.pushButton
-        self.setIDButton.clicked.connect(self.buttonClick)
-    
-    @pyqtSlot(str)
-    def show_message(self,msg):
-        self.ui.label.setText(msg)
         
-    def buttonClick(self):
-        self.runningThread.pub_message_by_thread()
+        self.ui.pushButton.clicked.connect(self.click_speed_up_button)
+        self.ui.pushButton_2.clicked.connect(self.click_speed_down_button)
+        self.ui.pushButton_3.clicked.connect(self.click_set_speed_button)
+        self.ui.pushButton_4.clicked.connect(self.click_speed_0_button)
+    
+    @pyqtSlot(float,float)
+    def show_message(self,positionX,positionY):
+        self.ui.label_2.setText(str(positionX))
+        self.ui.label_3.setText(str(positionY))
+        
+    def click_speed_up_button(self):
+        self.speed +=0.1;
+        self.runningThread.pub_message_by_thread(self.speed)
+        
+    def click_speed_down_button(self):
+        self.speed -=0.1;
+        self.runningThread.pub_message_by_thread(self.speed)
+    def click_set_speed_button(self):
+        setSpeed = self.ui.lineEdit.text()
+        self.speed = float(setSpeed)
+        self.runningThread.pub_message_by_thread(self.speed)
+    def click_speed_0_button(self):
+        self.speed =0.0;
+        self.runningThread.pub_message_by_thread(self.speed)
     
 if __name__ == "__main__":
     rclpy.init()
